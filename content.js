9188b9b0ca3571c32cebe265c4f3e23b4ef4cbcc
AlexShadow3 <alexandrebobis91@gmail.com>
2024-01-08 15:52:26 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
-document.body.innerHTML = document.body.innerHTML.replace(/agression/gi, 'EXTENSION');
+let keywords = []
+let keyword = "agression, viol, inceste, baise, pénétration, atouchement, chatte, bite, cul, porno, pédophile, abus, guerre, mort, assassinat, violence, meurtre, bombe, attentat, arme, amputation, éventrer, poignarder".split(",").forEach((word, i) => keywords[i] = word.trim())
 
-// agression, viol, inceste, baise, pénétration, atouchement, chatte, bite, cul, porno, pédophile, abus, guerre, mort, assassinat, violence, meurtre, bombe, attentat, arme, amputation, éventrer, poignarder
+console.log(keywords)
+
+for (let word of keywords) {
+    let regex = new RegExp(word, 'gi');
+    document.body.innerHTML = document.body.innerHTML.replace(regex, 'EXTENSION');
+}

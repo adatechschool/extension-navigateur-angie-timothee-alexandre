ffb04285a1c10f5a7b6b4cd7cf1e5ca39f1161c4
AlexShadow3 <alexandrebobis91@gmail.com>
2024-01-08 15:26:56 +0100

    Add list of keywords

---
+++ content.js
@@ -0,0 +1,3 @@
+document.body.innerHTML = document.body.innerHTML.replace(/agression/gi, 'EXTENSION');
+
+// agression, viol, inceste, baise, pénétration, atouchement, chatte, bite, cul, porno, pédophile, abus, guerre, mort, assassinat, violence, meurtre, bombe, attentat, arme, amputation, éventrer, poignarder
--- scripts/content.js
+++
@@ -1,2 +0,0 @@
-let yourname = prompt("What is your name?");
-alert("Salut " + yourname + " !")

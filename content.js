4d4ab6651cb959b3460deeacae6d028b6d47edd9
Angielacagole <angie.gillardet@gmail.com>
2024-01-09 15:08:05 +0100

    Version daubée bon courage

--- content.js
+++ content.js
@@ -6,26 +6,42 @@ const keywords = {
     "blessurePhysique": "amput, éventr, assassin, égorg, décapit, démembr, mutil, écorch, écartel, brûl, noy, pend, étrangl, poignard, empoison, écras, ébouill"
 }
 
-const parseTheme = ()=>{
-    const inputList = document.querySelectorAll('input')
+const parseTheme = () => {
+    const inputList = document.querySelectorAll('.theme-container input[type="checkbox"]');
     const listOfThemes = []
 
-    for (const theme of inputList){
-        if (theme.checked) {listOfThemes.push(theme.id)}
+    for (const theme of inputList) {
+        if (theme.checked) { listOfThemes.push(theme.id) }
     }
     return listOfThemes
 }
 
-async function hideTriggerWords (listOfThemes) // param = ['ViolenceSexuelle', 'guerre']
-{   
-    console.log('coucou')
-    for (let theme of listOfThemes) {
-        const themeWords = keywords[theme]
-        const wordsList = themeWords.split(",").map(word => word.trim())
-        let regex = new RegExp(wordsList.join('|'), 'gi');
-        document.body.innerHTML = document.body.innerHTML.replace(regex, 'EXTENSION');
-    }
-}
+// async function hideTriggerWords(listOfThemes) // param = ['ViolenceSexuelle', 'guerre']
+// {
+//     console.log('coucou')
+//     for (let theme of listOfThemes) {
+//         const themeWords = keywords[theme]
+//         const wordsList = themeWords.split(",").map(word => word.trim())
+//         let regex = new RegExp(wordsList.join('|'), 'gi');
+//         document.body.innerHTML = document.body.innerHTML.replace(regex, 'EXTENSION');
+//     }
+// }
 
+const wordsList = keywords.ViolenceSexuelle.split(",").map(word => word.trim())
+let regex = new RegExp(wordsList.join('|'), 'gi');
 
-hideTriggerWords(parseTheme())
+const input = document.querySelector('.theme-container > input[type="checkbox"]');
+
+input.addEventListener("change", () => {
+    console.log("BEAUTIFUL")
+    if (input.checked) {
+
+        var newDiv = document.createElement("div");
+
+        // Mettez à jour le contenu du nouvel élément avec le texte modifié
+        newDiv.innerHTML = document.body.innerHTML.replace(regex, 'EXTENSION');
+
+        // Remplacez le contenu de body par le nouvel élément div
+        document.body.innerHTML = newDiv.innerHTML;
+    };
+})

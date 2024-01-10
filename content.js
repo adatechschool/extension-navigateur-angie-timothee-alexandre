df26b6eb2ff3080f77e6d4dde3fca001835b5a46
AlexShadow3 <alexandrebobis91@gmail.com>
2024-01-10 14:39:14 +0100

    Update

--- content.js
+++ content.js
@@ -1,47 +1,36 @@
-//import { listOfThemes } from "./popup/popup"
-
-const keywords = {
-    "ViolenceSexuelle": "viol, inceste, pénétr, sex, atouch, porn, pédoph, abus, harc, dominat, agression",
-    "guerre": "guerre, mort, assassinat, violence, meurtre, bombe, attentat, arme, AK47, mitraill, flingue, pistolet, couteau, hache, sabre, tank, glock, uzi, carabine, p50, fusil, m16, explosion, escadron, commando, armée",
-    "blessurePhysique": "amput, éventr, assassin, égorg, décapit, démembr, mutil, écorch, écartel, brûl, noy, pend, étrangl, poignard, empoison, écras, ébouill"
-}
-
-const parseTheme = () => {
-    const inputList = document.querySelectorAll('.theme-container input[type="checkbox"]');
-    const listOfThemes = []
-
-    for (const theme of inputList) {
-        if (theme.checked) { listOfThemes.push(theme.id) }
+document.addEventListener("DOMContentLoaded", function () {
+    const keywords = {
+        "violenceSexuelle": "viol, inceste, pénétr, sex, atouch, porn, pédoph, abus, harc, dominat, agression",
+        "guerre": "guerre, mort, assassinat, violence, meurtre, bombe, attentat, arme, AK47, mitraill, flingue, pistolet, couteau, hache, sabre, tank, glock, uzi, carabine, p50, fusil, m16, explosion, escadron, commando, armée",
+        "blessurePhysique": "amput, éventr, assassin, égorg, décapit, démembr, mutil, écorch, écartel, brûl, noy, pend, étrangl, poignard, empoison, écras, ébouill"
     }
-    return listOfThemes
-}
 
-// async function hideTriggerWords(listOfThemes) // param = ['ViolenceSexuelle', 'guerre']
-// {
-//     console.log('coucou')
-//     for (let theme of listOfThemes) {
-//         const themeWords = keywords[theme]
-//         const wordsList = themeWords.split(",").map(word => word.trim())
-//         let regex = new RegExp(wordsList.join('|'), 'gi');
-//         document.body.innerHTML = document.body.innerHTML.replace(regex, 'EXTENSION');
-//     }
-// }
+    const parseTheme = () => {
+        const inputList = document.querySelectorAll('.theme-container > input[type="checkbox"]');
+        const listOfThemes = []
 
-const wordsList = keywords.ViolenceSexuelle.split(",").map(word => word.trim())
-let regex = new RegExp(wordsList.join('|'), 'gi');
+        for (const theme of inputList) {
+            if (theme.checked) { listOfThemes.push(theme.id) }
+        }
+        return listOfThemes
+    }
 
-const input = document.querySelector('.theme-container > input[type="checkbox"]');
+    const wordsList = keywords.violenceSexuelle.split(",").map(word => word.trim())
+    let regex = new RegExp(wordsList.join('|'), 'gi');
 
-input.addEventListener("change", () => {
-    console.log("BEAUTIFUL")
-    if (input.checked) {
+    let pageContentContainer = document.querySelector('.page-content');
 
-        var newDiv = document.createElement("div");
+    const input = document.querySelector('input[type="checkbox"]');
 
-        // Mettez à jour le contenu du nouvel élément avec le texte modifié
-        newDiv.innerHTML = document.body.innerHTML.replace(regex, 'EXTENSION');
-
-        // Remplacez le contenu de body par le nouvel élément div
-        document.body.innerHTML = newDiv.innerHTML;
-    };
-})
+    input.addEventListener("change", () => {
+        console.log("BEAUTIFUL")
+        if (input.checked) {
+            console.log("Checkbox is checked..");
+            const pageContent = document.body.innerText;
+            const modifiedContent = pageContent.replace(regex, 'EXTENSION');
+            pageContentContainer = modifiedContent.innerText;
+        } else {
+            console.log("Checkbox is not checked..");
+        }
+    });
+});
